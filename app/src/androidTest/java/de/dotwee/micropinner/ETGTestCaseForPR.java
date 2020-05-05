1d92a391251065c64a68406db029ce75c7f960c3
Ivan <iarcuschin@gmail.com>
2020-05-05 15:48:45 -0300

    Test case for PR

---
+++ app/src/androidTest/java/de/dotwee/micropinner/ETGTestCaseForPR.java
@@ -0,0 +1,130 @@
+package de.dotwee.micropinner;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static de.dotwee.micropinner.IsEqualTrimmingAndIgnoringCase.equalToTrimmingAndIgnoringCase;
+import static de.dotwee.micropinner.VisibleViewMatcher.isVisible;
+import static org.hamcrest.Matchers.*;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.action.GeneralLocation;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.view.View;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ETGTestCaseForPR {
+
+  @Rule
+  public ActivityTestRule<de.dotwee.micropinner.view.MainDialog> mActivityTestRule =
+      new ActivityTestRule<>(de.dotwee.micropinner.view.MainDialog.class);
+
+  @Test
+  public void myTestCase() {
+    System.out.println("Starting run of ETGTestCaseForPR");
+    ViewInteraction android_widget_Spinner =
+        onView(
+            allOf(
+                withId(R.id.spinnerVisibility),
+                isVisible(),
+                isDescendantOfA(withId(R.id.dialogContentView))));
+    android_widget_Spinner.perform(getLongClickAction());
+
+    Espresso.pressBackUnconditionally();
+
+    ViewInteraction android_widget_EditText =
+        onView(
+            allOf(
+                withId(R.id.editTextContent),
+                withTextOrHint(equalToTrimmingAndIgnoringCase("Content")),
+                isVisible(),
+                isDescendantOfA(withId(R.id.dialogContentView))));
+    android_widget_EditText.perform(replaceText("holoptychiid"));
+
+    ViewInteraction android_widget_Switch =
+        onView(
+            allOf(
+                withId(R.id.switchAdvanced),
+                isVisible(),
+                isDescendantOfA(
+                    allOf(
+                        withId(R.id.linearLayoutHeader),
+                        isDescendantOfA(withId(R.id.dialogHeaderView))))));
+    android_widget_Switch.perform(getClickAction());
+
+    ViewInteraction android_widget_LinearLayout =
+        onView(
+            allOf(
+                withId(R.id.linearLayoutHeader),
+                isVisible(),
+                hasDescendant(
+                    allOf(
+                        withId(R.id.dialogTitle),
+                        withTextOrHint(equalToTrimmingAndIgnoringCase("MicroPinner")))),
+                hasDescendant(withId(R.id.switchAdvanced)),
+                isDescendantOfA(withId(R.id.dialogHeaderView))));
+    android_widget_LinearLayout.perform(getLongClickAction());
+
+    ViewInteraction android_widget_CheckBox =
+        onView(
+            allOf(
+                withId(R.id.checkBoxShowActions),
+                withTextOrHint(equalToTrimmingAndIgnoringCase("Show notification actions")),
+                isVisible(),
+                isDescendantOfA(withId(R.id.dialogContentView))));
+    android_widget_CheckBox.perform(getClickAction());
+
+    ViewInteraction android_widget_EditText2 =
+        onView(
+            allOf(
+                withId(R.id.editTextTitle),
+                withTextOrHint(equalToTrimmingAndIgnoringCase("Title")),
+                isVisible(),
+                isDescendantOfA(withId(R.id.dialogContentView))));
+    android_widget_EditText2.perform(replaceText("tetraspgia"));
+
+    ViewInteraction android_widget_Button =
+        onView(
+            allOf(
+                withId(R.id.buttonPin),
+                withTextOrHint(equalToTrimmingAndIgnoringCase("PIN")),
+                isVisible(),
+                isDescendantOfA(withId(R.id.dialogFooterView))));
+    android_widget_Button.perform(getClickAction());
+  }
+
+  private static Matcher<View> withTextOrHint(final Matcher<String> stringMatcher) {
+    return anyOf(withText(stringMatcher), withHint(stringMatcher));
+  }
+
+  private ClickWithoutDisplayConstraint getClickAction() {
+    return new ClickWithoutDisplayConstraint(
+        Tap.SINGLE,
+        GeneralLocation.VISIBLE_CENTER,
+        Press.FINGER,
+        InputDevice.SOURCE_UNKNOWN,
+        MotionEvent.BUTTON_PRIMARY);
+  }
+
+  private ClickWithoutDisplayConstraint getLongClickAction() {
+    return new ClickWithoutDisplayConstraint(
+        Tap.LONG,
+        GeneralLocation.CENTER,
+        Press.FINGER,
+        InputDevice.SOURCE_UNKNOWN,
+        MotionEvent.BUTTON_PRIMARY);
+  }
+}
